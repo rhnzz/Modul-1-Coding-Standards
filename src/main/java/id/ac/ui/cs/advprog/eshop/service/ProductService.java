6ecac96ab55e421c2bfcadd7f7bac07103bee563
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-02-10 18:41:51 +0700

    add delete feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -6,6 +6,5 @@ import java.util.List;
 public interface ProductService {
     public Product create(Product product);
     public List<Product> findAll();
-    public Product findById(String productId);
-    public Product edit(Product product);
+    public void delete(String productId);
 }
